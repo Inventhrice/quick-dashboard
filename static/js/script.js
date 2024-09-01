5cd144c6e834e6af364741fecec0c110ef56c41c
Inventhrice <45127310+Inventhrice@users.noreply.github.com>
2024-08-31 22:11:59 -0400

    Added a fully functional upload of file to populate data

--- static/js/script.js
+++ static/js/script.js
@@ -10,9 +10,11 @@ function loadJSON(){
         "load",
         () => {
             // this will then display a text file
-            console.log(reader.result);
             const obj = JSON.parse(reader.result);
-            console.log(obj)
+            console.log(obj);
+
+            loadJSONHelper_loadTitle(obj.title);
+            loadJSONHelper_loadTasks(obj.tasks);
         },
         false,
     );
@@ -20,3 +22,24 @@ function loadJSON(){
     //console.log(listTasks)
 }
 
+function loadJSONHelper_loadTitle(title){
+    document.getElementById("applicationTitle").innerHTML = title;
+}
+
+function loadJSONHelper_loadTasks(tasks){
+    console.log(tasks.length)
+    document.getElementById("checklist").innerHTML = "";
+    for(let i = 0; i < tasks.length; i++){
+        theTaskInQuestion = tasks[i]
+        console.log(theTaskInQuestion)
+        template = `<div id="ID" class="checklistItem list-group list-group-item-action ps-2 pt-2 mb-2" data-bs-toggle="modal" data-bs-target="#taskID">`
+        template = template.replaceAll("ID", theTaskInQuestion.id);
+
+        template += "\n<h5>" + theTaskInQuestion.taskTitle + "</h5>"
+                + "\n<p>" + theTaskInQuestion.assignee + "</p>" + "\n</div>\n"
+
+        document.getElementById("checklist").innerHTML += template;        
+    }
+    
+
+}
