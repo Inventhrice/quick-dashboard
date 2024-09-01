a452298d014e109f6b26411fcbf451db28065784
Inventhrice <45127310+Inventhrice@users.noreply.github.com>
2024-09-01 11:01:27 -0400

    Added assignee info to detailed view

--- static/js/script.js
+++ static/js/script.js
@@ -60,16 +60,16 @@ function updateTasks(tasks){
         completeTaskTemplate = `<div class="modal fade" id="taskID" tabindex="-1" aria-labelledby="taskID" aria-hidden="true">`.replaceAll("ID", theTaskInQuestion.id)
         completeTaskTemplate += `<div class="modal-dialog">\n<div class="modalBackground modal-content">\n<div class="modal-header">`
         completeTaskTemplate += `<h1 class="modal-title fs-5" id="modalLabel">TASKTITLE</h1>`.replaceAll("TASKTITLE", theTaskInQuestion.taskTitle)
-        completeTaskTemplate += `<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div>`
-
+        completeTaskTemplate += `<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body">`
+        completeTaskTemplate += `<p><i>Assigned to: </i>` + theTaskInQuestion.assignee + `</p>`
         if(theTaskInQuestion.description == ""){
-            completeTaskTemplate += `<div class="modal-body">TASKDESCRIPTION</div>`.replaceAll("TASKDESCRIPTION", "<p class=\"fst-italic\">No Description Provided</p>")
+            completeTaskTemplate += `<p class="fst-italic">No Description Provided</p>`
         }
         else{
-            completeTaskTemplate += `<div class="modal-body">TASKDESCRIPTION</div>`.replaceAll("TASKDESCRIPTION", theTaskInQuestion.description).replaceAll("\n","<br>")
+            completeTaskTemplate += theTaskInQuestion.description.replaceAll("\n","<br>")
         }
 
-        completeTaskTemplate += `<div class="modal-footer">\n<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>\n</div>\n</div>\n</div>\n</div>`
+        completeTaskTemplate += `</div>\n<div class="modal-footer">\n<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>\n</div>\n</div>\n</div>\n</div>`
 
         document.getElementById("listTasks").innerHTML += completeTaskTemplate;
     }
